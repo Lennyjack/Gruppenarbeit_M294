2963a71432c71ec1fd09ac26192313c09a4e9386
Lennyjack <lenny.brun@student.ipso.ch>
2024-11-07 16:21:02 +0100

    Serververbindung

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-    // Eingabedaten abrufen
-    const customerName = document.getElementById('customerName').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const priority = document.getElementById('priority').value;
-    const service = document.getElementById('service').value;
-    // Berechnung des Abholdatums
-const pickupDate = calculatePickupDate(priority);
-
 function validateEmail(email) {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailPattern.test(email);
@@ -21,20 +12,30 @@ function validatePhone(phone) {
 function handleFormSubmit(event) {
     event.preventDefault();
 
+    
+    // Eingabedaten abrufen
+    const name = document.getElementById('customerName').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const priority = document.getElementById('priority').value;
+    const service = document.getElementById('service').value;
+    // Berechnung des Abholdatums
+    const pickup_Date = "2024-11-02T18:12:43.511Z";   // calculatePickupDate(priority);
+
     // Validierung und Senden der Daten an den Server
-    if (validateEmail(email) && validatePhone(phone)) {
+    //if (validateEmail(email) && validatePhone(phone)) {
         const data = {
-            customerName,
+            name,
             email,
             phone,
             priority,
             service,
-            pickupDate,
-            submissionDate: new Date().toISOString().split('T')[0]
+            pickup_Date,
+            submission_Date: new Date().toISOString().split('T')[0]
         };
 
         // Server-Aufruf mit angepasstem API-Endpunkt
-        fetch('http://localhost:5000/registration', {
+        fetch('http://localhost:5000/api/registration', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -51,7 +52,7 @@ function handleFormSubmit(event) {
             }
         })
         .catch(error => console.error('Error:', error));
-    } else {
-        alert('Bitte überprüfen Sie Ihre Eingaben.');
-    }
+    //} else {
+    //    alert('Bitte überprüfen Sie Ihre Eingaben.');
+    //}
 }
